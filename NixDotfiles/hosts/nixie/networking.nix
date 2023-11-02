6b4398e57dc70b3d93ec174443b50d003e237361
Emily3403 <seebeckemily3403@gmail.com>
2023-11-02 10:11:26 +0100

    Add dmz IP

--- NixDotfiles/hosts/nixie/networking.nix
+++ NixDotfiles/hosts/nixie/networking.nix
@@ -35,6 +35,13 @@
       }];
     };
 
+    interfaces.eno3 = {
+      ipv4.addresses = [{
+        address = "130.149.220.240";
+        prefixLength = 25;
+      }];
+    };
+
     firewall = {
       enable = true;
       allowedTCPPorts = [ 22 25 80 443 465 587 993 ];
